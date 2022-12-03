ab563b40e71f81c5eecb1cd97ce6b1e8b05cfb6a
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2022-12-03 16:35:51 -0600

    Day 3 part 2

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -6,34 +6,46 @@ const parseInput = (rawInput: string): string[] => {
   return lines;
 };
 
+function getItemPriority(item?: string): number {
+  if (!item) return 0;
+
+  // use ascii A being 65 and a being 97 to speed this up
+  const charCode = item.charCodeAt(0);
+  // `A` is `65` while `a` is `97`
+  return charCode < 97 ? charCode - 38 : charCode - 96;
+}
+
 const part1 = (rawInput: string): number => {
   const input = parseInput(rawInput);
 
   const sum = input.reduce((rucksackSum: number, rucksack: string): number => {
     const rucksackItems = rucksack.split('');
-    const [compartment1, compartment2] = _.chunk(
+    const compartments = _.chunk(
         rucksackItems,
         rucksackItems.length / 2
     );
-    const items = _.intersection(compartment1, compartment2);
+    const items = _.intersection(...compartments);
 
     return rucksackSum + items.reduce((compartmentSum: number, item: string): number => {
-      // use ascii A being 65 and a being 97 to speed this up
-      const charCode = item.charCodeAt(0);
-      // `A` is `65` while `a` is `97`
-      const priority = charCode < 97 ? charCode - 38 : charCode - 96;
-
-      return compartmentSum + priority;
+      return compartmentSum + getItemPriority(item);
     }, 0);
   }, 0);
 
   return sum;
 };
 
-const part2 = (rawInput: string) => {
+const part2 = (rawInput: string): number => {
   const input = parseInput(rawInput);
+  const rucksacks = input.map((rucksack: string): string[] => rucksack.split(''));
+  const groups = _.chunk(rucksacks, 3);
 
-  return;
+  const sum = groups.reduce((acc: number, groupRucksacks: string[][]): number => {
+    const badge = _.first(_.intersection(...groupRucksacks));
+
+    return acc + getItemPriority(badge);
+  }, 0);
+
+  return sum;
 };
 
 run({
@@ -55,10 +67,17 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+        `,
+        expected: 70,
+      },
     ],
     solution: part2,
   },
