4f5cf5f151c2db4e1afc7767dff0e41f7ae50ce9
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2022-12-08 10:51:51 -0600

    Day 8 part 2

--- src/day08/index.ts
+++ src/day08/index.ts
@@ -86,6 +86,77 @@ function getVisibleTrees(trees: number[][]): number {
   return visible;
 }
 
+function getTreeScore(trees: number[][], tree: Tree): number {
+  const { row, col, height } = tree;
+  let visibleUp = 0;
+  let visibleDown = 0;
+  let visibleLeft = 0;
+  let visibleRight = 0;
+
+  // up
+  for (let index = row - 1; index >= 0; index--) {
+    if (trees[index][col] >= height) {
+      visibleUp += 1;
+      break;
+    }
+    visibleUp += 1;
+  }
+
+  // down
+  for (let index = row + 1; index < trees.length; index++) {
+    if (trees[index][col] >= height) {
+      visibleDown += 1;
+      break;
+    }
+    visibleDown += 1;
+  }
+
+  // left
+  for (let index = col - 1; index >= 0; index--) {
+    if (trees[row][index] >= height) {
+      visibleLeft += 1;
+      break;
+    }
+    visibleLeft += 1;
+  }
+
+  // right
+  for (let index = col + 1; index < trees[row].length; index++) {
+    if (trees[row][index] >= height) {
+      visibleRight += 1;
+      break;
+    }
+    visibleRight += 1;
+  }
+
+  return visibleUp * visibleDown * visibleLeft * visibleRight;
+}
+
+function getHighestScenicScore(trees: number[][]): number {
+  const score = trees.reduce((rowAcc: number, row: number[], rowIndex: number): number => {
+    // first and last rows always will have score of 0 due to outside edge
+    if (rowIndex === 0 || rowIndex === trees.length - 1) return rowAcc;
+
+    return row.reduce((colAcc: number, col: number, colIndex: number): number => {
+      // first and last columns always have score of 0 due to outside edge
+      if (colIndex === 0 || colIndex === row.length - 1) return colAcc;
+
+      const score = getTreeScore(
+        trees,
+        { row: rowIndex, col: colIndex, height: col }
+      );
+
+      if (score > colAcc) {
+        return score;
+      }
+
+      return colAcc;
+    }, rowAcc);
+  }, 0);
+
+  return score;
+}
+
 const part1 = (rawInput: string): number => {
   const input = parseInput(rawInput);
   const visible = getVisibleTrees(input);
@@ -93,10 +164,11 @@ const part1 = (rawInput: string): number => {
   return visible;
 };
 
-const part2 = (rawInput: string) => {
+const part2 = (rawInput: string): number => {
   const input = parseInput(rawInput);
+  const score = getHighestScenicScore(input);
 
-  return;
+  return score;
 };
 
 run({
@@ -117,10 +189,16 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+          30373
+          25512
+          65332
+          33549
+          35390
+        `,
+        expected: 8,
+      },
     ],
     solution: part2,
   },
