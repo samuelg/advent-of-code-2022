891223e0c3087d367839bf57015b78c9441ed360
Samuel Fortier-Galarneau <samuel.galarneau@gmail.com>
2022-12-09 10:08:04 -0600

    Day 9 part 2

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
-[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](src/day09)
+[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
@@ -125,9 +125,9 @@ Both parts: 8.574308ms
 
 ```
 Day 09
-Time part 1: 6.721ms
-Time part 2: -
-Both parts: 6.72058ms
+Time part 1: 7.818ms
+Time part 2: 2.708ms
+Both parts: 10.526216999999999ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 17/50
-Total time: 59.068ms
+Total stars: 18/50
+Total time: 62.874ms
 ```
 
 <!--/RESULTS-->
--- src/day09/index.ts
+++ src/day09/index.ts
@@ -1,4 +1,5 @@
 import run from "aocrunner";
+import _ from "lodash";
 
 enum Direction {
   Left = "L",
@@ -32,7 +33,8 @@ const parseInput = (rawInput: string): Move[] => {
   return moves;
 };
 
-function touches(head: Position, tail: Position): boolean {
+function touches(head: Position | undefined, tail: Position): boolean {
+  if (!head) return false;
   if (head.x < tail.x && tail.x - head.x > 1) return false;
   if (head.x > tail.x && head.x - tail.x > 1) return false;
   if (head.y < tail.y && tail.y - head.y > 1) return false;
@@ -41,7 +43,8 @@ function touches(head: Position, tail: Position): boolean {
   return true;
 }
 
-function moveHead(head: Position, direction: Direction): void {
+function moveHead(head: Position | undefined, direction: Direction): void {
+  if (!head) return;
   if (direction === Direction.Up) head.y += 1;
   if (direction === Direction.Down) head.y -= 1;
   if (direction === Direction.Right) head.x += 1;
@@ -55,18 +58,28 @@ function moveTail(head: Position, tail: Position): void {
   if (head.y > tail.y) tail.y += 1;
 }
 
-function processMoves(moves: Move[]): number {
-  const head: Position = { x: 0, y: 0 };
-  const tail: Position = { x: 0, y: 0 };
+function processMoves(moves: Move[], length: number): number {
+  // head at position 0, tail at position length - 1
+  const rope = _.range(length).map((): Position => ({ x: 0, y: 0 }));
 
   // keep track of unique tail positions
   const positions = moves.reduce((acc: Set<string>, move: Move): Set<string> => {
     for (let place = 0; place < move.places; place++) {
-      moveHead(head, move.direction);
+      moveHead(_.head(rope), move.direction);
 
-      if (!touches(head, tail)) {
-        moveTail(head, tail);
-        acc.add(`${tail.x}|${tail.y}`);
+      for (let tailIndex = 1; tailIndex < length; tailIndex++) {
+        const previous = rope[tailIndex - 1];
+        const tail = rope[tailIndex];
+
+        // if the current tail touches, all other tails will as well
+        if (touches(previous, tail)) break;
+
+        moveTail(previous, tail);
+
+        // this is the last knot and therefore the true tail
+        if (tailIndex === length - 1) {
+          acc.add(`${tail.x}|${tail.y}`);
+        }
       }
     }
 
@@ -78,15 +91,18 @@ function processMoves(moves: Move[]): number {
 
 const part1 = (rawInput: string): number => {
   const moves = parseInput(rawInput);
-  const tailPositions = processMoves(moves);
+  // for part 1 the rope length is 2
+  const tailPositions = processMoves(moves, 2);
 
   return tailPositions;
 };
 
-const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+const part2 = (rawInput: string): number => {
+  const moves = parseInput(rawInput);
+  // for part 2 the rope length is 10
+  const tailPositions = processMoves(moves, 10);
 
-  return;
+  return tailPositions;
 };
 
 run({
@@ -110,10 +126,32 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+          R 4
+          U 4
+          L 3
+          D 1
+          R 4
+          D 1
+          L 5
+          R 2
+        `,
+        expected: 1,
+      },
+      {
+        input: `
+          R 5
+          U 8
+          L 8
+          D 3
+          R 17
+          D 10
+          L 25
+          U 20
+        `,
+        expected: 36,
+      },
     ],
     solution: part2,
   },
